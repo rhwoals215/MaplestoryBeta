1921c91d742ec7696368981850e32638ee5aaab4
Jaemin Ko <rhwoals215@gmail.com>
2017-05-15 10:19:39 +0900

    added 1st monster, fixed menu

--- maplestorybeta.py
+++ maplestorybeta.py
@@ -23,16 +23,18 @@ choice=raw_input("press enter to continue")
 lvl=1
 meso=0
 sp=0
-exp=1000
+exp=0
 str1=5
 dex1=5
 luk1=5
 int1=5
 dmg=5
+word=""
 if choice=="administrator":
     lvl=100
     meso=1000000000
 playing=True
+hunting=True
 os.system('clear')
 while playing:
     print("-----------------------------------------------------")
@@ -51,10 +53,11 @@ while playing:
     print("-----------------------------------------------------")
     print("")
     print("")
-    choice=int(input("Select"))
-    if choice==8:
+    hunting=True
+    choice=raw_input("Select")
+    if choice=="8":
         break
-    if choice==1:
+    if choice=="1":
         while exp>(lvl^2+2*lvl):
             exp-=(lvl^2+2*lvl)
             lvl+=1
@@ -119,35 +122,71 @@ while playing:
         print("")
         choice=raw_input("Press enter to go back")
         os.system('clear')
-    if choice==2:
+    if choice=="2":
         os.system('clear')
-        print("-----------------------------------------------------")
-        print("-----------------------Hunting-----------------------")
-        print("-----------------------------------------------------")
-        print("-----------------------OPTIONS-----------------------")
-        print("-----------------------------------------------------")
-        print("-1.-Hunt-Snails-(LV1-5)------------------------------")
-        print("-2.-Hunt-Slimes-(LV3-7)------------------------------")
-        print("-3.-Hunt-Pigs-(LV5-10)-------------------------------")
-        print("-4.-Hunt-Mushrooms-(LV7-10)--------------------------")
-        print("-----------------------------------------------------")
-        print("-----------------------------------------------------")
-        print("-----------------------------------------------------")
-        print("-----------------------------------------------------")
-        print("-----------------------------------------------------")
-        print("")
-        print("")
-        print("")
-        print("")
-        choice=raw_input("Press enter to go back")
-        if choice==1:
-            chance=random.randint(1,str1+lvl)
-            if chance==1:
-                print("you died...")
-
-
-
-
-
-
-        os.system('clear')
+        while hunting:
+            print("-----------------------------------------------------")
+            print("-----------------------Hunting-----------------------")
+            print("-----------------------------------------------------")
+            print("-----------------------OPTIONS-----------------------")
+            print("-----------------------------------------------------")
+            print("-1.-Hunt-Snails-(LV1-5)------------------------------")
+            print("-2.-Hunt-Slimes-(LV3-7)------------------------------")
+            print("-3.-Hunt-Pigs-(LV5-10)-------------------------------")
+            print("-4.-Hunt-Mushrooms-(LV7-10)--------------------------")
+            print("-5.-Back---------------------------------------------")
+            print("-----------------------------------------------------")
+            print("-----------------------------------------------------")
+            print("-----------------------------------------------------")
+            print("-----------------------------------------------------")
+            print("")
+            print("")
+            print("")
+            print("")
+            choice=raw_input("Select")
+            if choice=="5":
+                hunting=False
+            if choice=="1":
+                if dex%2=1:
+                    atk=dmg+str1+(dex+1)/2+lvl
+                else:
+                    atk=dmg+str1+dex/2+lvl
+                chance=random.randint(1,atk)
+                if chance==1:
+                    print("you died...")
+                    print("")
+                    choice=raw_input("please press enter to continue.")
+                    if exp>10:
+                        exp-=10
+                else:
+                    random1=random.randint(1,10)
+                    if 1<=random1<=3:
+                        word="small amount"
+                        exp+=1
+                    if 4<=random1<=7:
+                        word="lot"
+                        exp+=2
+                    if 8<=random1<=9:
+                        word="bunch"
+                        exp+=3
+                    if random1==10:
+                        word="crap ton"
+                        exp+=4
+                    print("You defeated a "+word+" of Snails.")
+                    if 1<=random1<=3:
+                        word="1"
+                    if 4<=random1<=7:
+                        word="2"
+                    if 8<=random1<=9:
+                        word="3"
+                    if random1==10:
+                        word="4"
+                    print("You got "+word+" exp.")
+                    while exp>(lvl^2+2*lvl):
+                        exp-=(lvl^2+2*lvl)
+                        lvl+=1
+                        sp+=5
+                        print("You leveled up!!!")
+                    print("")
+                    choice=raw_input("please press enter to continue.")
+            os.system('clear')
